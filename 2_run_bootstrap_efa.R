a651e0ff92152ec2e8915a591bdac973a537a85c
David Evans <dwe@backpainclinic.co.uk>
2023-06-11 09:36:12 +0100

    Allow for additional output from bootstrap EFA

--- 2_run_bootstrap_efa.R
+++ 2_run_bootstrap_efa.R
@@ -28,15 +28,6 @@ n_factors_inpain <- determine_factors$n_factors_inpain
 n_factors_painfree <- determine_factors$n_factors_painfree
 inpain_suggested_factors <- determine_factors$inpain_suggested_factors
 painfree_suggested_factors <- determine_factors$painfree_suggested_factors
-common_items <- readRDS("common_items.rds")
-prev_num_common_items <- common_items$num_common_items
-
-#Check number of common items previously identified
-if (file.exists("num_common_items.rds")) {
-  prev_num_common_items <- readRDS("num_common_items.rds")
-} else {
-  prev_num_common_items <- 0
-}
 
 # Run EFA with the fixed number of factors to establish the factor structure
 efa_full_inpain <- fa(inpain.df, nfactors = n_factors_inpain, rotate = "oblimin", fm = "minres")
@@ -53,10 +44,10 @@ perform_efa <- function(data, target_loadings, iter) {
 }
 
 # Function to perform Procrustes rotation
-perform_procrustes <- function(f, target_loadings, iter) {
+perform_procrustes <- function(f, target_loadings, iter, rotation_type = 'oblique', verbose = TRUE) {
   # Perform Procrustes rotation on the factor loadings
   procrustes_result <- tryCatch({
-    EFA.dimensions::PROCRUSTES(loadings = f, target = target_loadings, type = 'oblique', verbose = TRUE)
+    EFA.dimensions::PROCRUSTES(loadings = f, target = target_loadings, type = rotation_type, verbose = verbose)
   }, error = function(e) {
     print(paste("ERROR in Procrustes rotation at iteration ", iter, ": ", e$message))
     return(NULL)
@@ -68,18 +59,19 @@ perform_procrustes <- function(f, target_loadings, iter) {
   # If Procrustes rotation was successful, proceed with the analysis
   if(!is.null(procrustes_result)){
     # Print the factor loadings after Procrustes rotation
-    print(paste("Factor loadings after Procrustes rotation at iteration ", iter, ":"))
-    print(procrustes_result$loadingsPROC)
-    
-    # Return the rotated loadings
-    return(procrustes_result$loadingsPROC)
+    if(verbose) {
+      print(paste("Factor loadings after Procrustes rotation at iteration ", iter, ":"))
+      print(procrustes_result$loadingsPROC)
+    }
+    # Return the full Procrustes result instead of just the rotated loadings
+    return(procrustes_result)
   } else {
     return(NULL)
   }
 }
 
 # Define a function to perform EFA and extract the suggested number of factors
-efa_func <- function(data, target_loadings, n_iterations) {
+efa_func <- function(data, target_loadings, n_iterations, rotation_type = 'oblique', verbose = TRUE) {
   iter = 1
   repeat {
     # Print the iteration number
@@ -94,15 +86,23 @@ efa_func <- function(data, target_loadings, n_iterations) {
     # If EFA was successful, proceed with the analysis
     if(!is.null(efa)){
       # Perform Procrustes rotation on the factor loadings
-      efa$loadings <- perform_procrustes(efa$loadings, target_loadings, iter)
+      procrustes_result <- perform_procrustes(efa$loadings, target_loadings, iter, rotation_type, verbose)
       
       # If Procrustes rotation was successful, proceed with the analysis
-      if(!is.null(efa$loadings)){
+      if(!is.null(procrustes_result)){
         # Convert the factor loadings to a data frame and return the result
-        loadings_df <- as.data.frame(efa$loadings)
-        rownames(loadings_df) <- rownames(efa$loadings)
-        colnames(loadings_df) <- colnames(efa$loadings)
-        return(loadings_df)
+        loadings_df <- as.data.frame(procrustes_result$loadingsPROC)
+        rownames(loadings_df) <- rownames(procrustes_result$loadingsPROC)
+        colnames(loadings_df) <- colnames(procrustes_result$loadingsPROC)
+        
+        # Include additional results from Procrustes rotation in the output
+        result_list <- list(
+          'loadings_df' = loadings_df,
+          'congruence' = procrustes_result$congruence,
+          'rmsr' = procrustes_result$rmsr,
+          'residmat' = procrustes_result$residmat
+        )
+        return(result_list)
       }
     }
     # Increment the iteration counter
@@ -125,7 +125,12 @@ bootstrap_efa <- function(data, n_iterations, target_loadings) {
     print(paste("Bootstrap EFA iteration ", i))
     efa_results <- efa_func(data, target_loadings, n_iterations)
     if(!is.null(efa_results)){
-      results_list[[i]] <- efa_results
+      results_list[[i]] <- list(
+        'loadings_df' = efa_results$loadings_df,
+        'congruence' = efa_results$congruence,
+        'rmsr' = efa_results$rmsr,
+        'residmat' = efa_results$residmat
+      )
     }
   }
   # Filter out NULL values from the list
@@ -144,7 +149,7 @@ saveRDS(painfree_efa, "painfree_efa.rds")
 analyze_loadings <- function(loadings_list, group_name) {
   loadings_df <- do.call(rbind, lapply(loadings_list, function(x) {
     if(!is.null(x)){
-      loadings = x
+      loadings = x$loadings_df
       if(all(is.na(loadings))){
         print(paste("All values in loadings are NA for group", group_name))
         return(NULL)
